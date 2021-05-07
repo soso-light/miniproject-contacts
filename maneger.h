ff9aab8a4609deaab4d08c4246d3c29ff1c2b808
soso-light <80017031+soso-light@users.noreply.github.com>
2021-05-07 22:38:38 +0900

    git commit -m "make programming files"

---
+++ maneger.h
@@ -0,0 +1,6 @@
+#include "contact.h"
+
+void listContact(Contact *p, int count);
+int selectDataNumber(Contact *p, int count);
+void saveContactData(Contact p, int count);
+int loadContactData(Contact p);
