fd374251fbb9e5ba425b8de2a97d6bb1768348f2
soso-light <80017031+soso-light@users.noreply.github.com>
2021-05-07 23:56:14 +0900

    git commit -m "update manager.h"

--- manager.c
+++ manager.c
@@ -1,6 +1,51 @@
 #include "manager.h"
 
-void listContact(Contact *p, int count);
-int selectDataNumber(Contact *p, int count);
-void saveContactData(Contact p, int count);
-int loadContactData(Contact p);
+void listContact(Contact *p, int count){
+	printf("\nNo number\t\tName\n");
+	printf("=================================================\n");
+	for(int i =0; i <count ; i++){
+		if(p[i].number == -1) continue;
+		printf("%2d ", i+1);
+		readProduct(p[i]);
+	}
+	printf("\n");
+}
+
+int selectDataNumber(Contact *p, int count){
+    int idx;
+	listContact(p, count);
+	printf("번호는?(취소: 0)");
+	scanf("%d",&idx);
+	return idx;
+}
+
+void saveContactData(Contact *p, int count){
+	FILE *fp;
+	fp = fopen("contact.txt", "wt");
+	for(int i = 0; i < count; i++){
+		if(p[i].number == -1) continue;
+		fprintf(fp, "%s %d\n", p[i].name, p[i].number);
+	}
+	fclose(fp);
+	printf("=> 저장됨! \n");
+
+}
+
+int loadContactData(Contact *p){
+	int i=0;
+	FILE *fp;
+	fp = fopen("contact.txt","rt");
+	if(fp == NULL) {
+		    printf("=> 파일 없음\n");
+		    return 0;
+	}
+	for(; i < 100; i++){
+		fscanf(fp, "%s", p[i].name);
+		if(feof(fp)) break;
+		fscanf(fp, "%d", &p[i].number);
+	}
+	fclose(fp);
+	printf("=> 로딩 성공!\n");
+	return i;
+
+}
--- manager.h
+++ manager.h
@@ -2,5 +2,5 @@
 
 void listContact(Contact *p, int count);
 int selectDataNumber(Contact *p, int count);
-void saveContactData(Contact p, int count);
-int loadContactData(Contact p);
+void saveContactData(Contact *p, int count);
+int loadContactData(Contact *p);
