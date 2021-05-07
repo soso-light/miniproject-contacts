d3d6deb1d4d048293be4501e35f5de9493565cfa
soso-light <80017031+soso-light@users.noreply.github.com>
2021-05-07 22:49:13 +0900

    commit -m "change filename"

---
+++ manager.c
@@ -0,0 +1,6 @@
+#include "maneger.h"
+
+void listContact(Contact *p, int count);
+int selectDataNumber(Contact *p, int count);
+void saveContactData(Contact p, int count);
+int loadContactData(Contact p);
